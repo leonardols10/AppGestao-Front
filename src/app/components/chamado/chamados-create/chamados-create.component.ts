aca4d81e4f120944ff2df0e84d833cf4903f7f6c
leonardols10 <leonardosilvalima40@gmail.com>
2022-11-20 21:36:37 -0300

    validacao form

--- src/app/components/chamado/chamados-create/chamados-create.component.ts
+++ src/app/components/chamado/chamados-create/chamados-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chamados-create',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadosCreateComponent implements OnInit {
 
+
+  prioridade: FormControl = new FormControl(null, [ Validators.required]);
+  status: FormControl = new FormControl(null, [ Validators.required]);
+  titulo: FormControl = new FormControl(null, [ Validators.required]);
+  descricao: FormControl = new FormControl(null, [ Validators.required]);
+  colaborador: FormControl = new FormControl(null, [ Validators.required]);
+  cliente: FormControl = new FormControl(null, [ Validators.required]);
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid &&
+                this.status.valid &&
+                      this.titulo.valid &&
+                           this.descricao.valid &&
+                                this.colaborador.valid &&
+                                     this.cliente.valid 
   }
 
 }
